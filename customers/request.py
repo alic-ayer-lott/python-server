bb0cf398606a0ec9ba557aad69c62d2bb1c368c8
Alicia AyersLott <amayers92@gmail.com>
2021-10-18 14:10:26 -0500

    added SQL database

--- customers/request.py
+++ customers/request.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Customer
+
 CUSTOMERS = [
     {
         "id": 1,
@@ -14,16 +18,52 @@ CUSTOMERS = [
 ]
 
 def get_all_customers():
-    return CUSTOMERS
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        """)
+
+        customers = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'], row['password'])
+            customers.append(customer.__dict__)
+    
+    return json.dumps (customers)
+
 
 def get_single_customer(id):
-    requested_customer = None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    for customer in CUSTOMERS:
-        if customer["id"] ==id:
-            requested_customer = customer
-    
-    return requested_customer
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        FROM customer a
+        WHERE a.id = ?
+        """, (id, ))
+
+        data = db_cursor.fetchone()
+
+        customer = Customer(data['id'], data['name'], data['address'], data['email'], data['password'])
+
+        return json.dumps(customer.__dict__)
 
 
 def create_customer(customer):
